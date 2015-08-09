9b1d029a7b842778a7c2406909e69462765fd52c
myxiong <myxiong@uwaterloo.ca>
2015-08-09 17:11:08 -0400

    Closet 1.0 (the very basics)

--- app/src/main/java/com/taro/tusk/Closet.java
+++ app/src/main/java/com/taro/tusk/Closet.java
@@ -1,29 +1,33 @@
 package com.taro.tusk;
 
+import android.content.Context;
 import android.content.Intent;
 import android.support.v7.app.ActionBarActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.Toast;
 import com.taro.tusk.closet.type.ArticleType;
 import com.taro.tusk.closet.OpenCloset;
 
 public class Closet extends ActionBarActivity {
 
     ArticleType currentArticle;
+    //ArticleType article = OpenCloset.tShirt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_camera);
+        setContentView(R.layout.activity_closet);
     }
 
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_camera, menu);
+        getMenuInflater().inflate(R.menu.menu_closet, menu);
         return true;
     }
 
@@ -32,33 +36,62 @@ public class Closet extends ActionBarActivity {
     }
 
     public void changeEnabled (View v){
-        ArticleType article = findIDOfArticle(v);
-        if(article.getIsEnabled()){
-            article.setIsEnabled(false);
+    ArticleType current = findIDOfArticle(v);
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        if(current.getIsEnabled()){
+            current.setIsEnabled(false);
         }else{
-            article.setIsEnabled(true);
+            current.setIsEnabled(true);
         }
+        CharSequence text = current + "isEnabled" + Boolean.toString(current.getIsEnabled());
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public void changeNotified (View v){
-        ArticleType article = findIDOfArticle(v);
-        if(article.getIsNotified()){
-            article.setIsNotified(false);
+        ArticleType current = findIDOfArticle(v);
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+
+        if(current.getIsNotified()){
+            current.setIsNotified(false);
         }else{
-            article.setIsNotified(true);
+            current.setIsNotified(true);
         }
+
+        CharSequence text = current + "isNotified" + Boolean.toString(current.getIsNotified());
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
+    }
+
+    public void tempMethod (View v){
+        ArticleType current = findIDOfArticle(v);
+
+        Context context = getApplicationContext();
+        int duration = Toast.LENGTH_SHORT;
+        CharSequence text = "isNotified" + Boolean.toString(current.getIsNotified())+
+                "isEnabled" + Boolean.toString(current.getIsEnabled());
+        Toast toast = Toast.makeText(context, text, duration);
+        toast.show();
     }
 
     public ArticleType findIDOfArticle (View v){
         ArticleType findID;
-
-        switch (v.getId()){
+        // is this how to get id of (parent) layout??
+        switch (((View) v.getParent()).getId()){
             case R.id.t_shirt: findID = OpenCloset.tShirt;
                 break;
-            default: findID = OpenCloset.skirt;
+            case R.id.skirt: findID = OpenCloset.skirt;
                 break;
-            //default:
-               // break;
+            case R.id.tank: findID = OpenCloset.tank;
+                break;
+            case R.id.shorts: findID = OpenCloset.shorts;
+                break;
+            default: findID = OpenCloset.skirt; //change this to what?
+                break;
         }
         return findID;
     }
--- app/src/main/java/com/taro/tusk/closet/OpenCloset.java
+++ app/src/main/java/com/taro/tusk/closet/OpenCloset.java
@@ -8,9 +8,9 @@ import com.taro.tusk.closet.type.Top;
  * Created by Michelle Xiong on 04/08/2015.
  */
 public class OpenCloset {
-        public static ArticleType tShirt = new Top(true,true);
-        public static ArticleType tank = new Top (true,false);
-        public static ArticleType shorts = new Bottom(false,true);
-        public static ArticleType skirt = new Bottom (false,false);
+    public static ArticleType tShirt = new Top(true,true);
+    public static ArticleType tank = new Top (true,true);
+    public static ArticleType shorts = new Bottom(false,true);
+    public static ArticleType skirt = new Bottom (false,false);
 
 }
